12678666694023456ae4f80b9d318354fcc6f488
Andrés Achury <aachury@serempre.com>
2021-04-02 16:00:09 -0500

    Feat: Section one from home page CSS-HTML

--- src/screens/Blog/home/HomeScreen.js
+++ src/screens/Blog/home/HomeScreen.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import cta from '../../../assets/cta.jpg';
 
 const HomeScreen = () => {
   return (
-    <div>
-      <h1>Home Screen</h1>
-    </div>
+    <main className="blog-screen__main">
+      <section className="blog-screen__s__one">
+        <img src={cta} alt="Blog" />
+        <div className="blog-screen__s__one__div">
+          <h1>
+            {' '}
+            Aprende, descubre <br /> y aplica como manejar tus emociones
+          </h1>
+          <p>
+            Reprehenderit nulla est minim consectetur do sit adipisicing tempor
+            commodo eiusmod aute dolore do in. Officia cupidatat laboris nulla
+            mollit cillum nostrud minim pariatur proident est.
+          </p>
+          <button>Quiero Aprender</button>
+        </div>
+      </section>
+    </main>
   );
 };
 
