844e6de9c90136e897f2404bdebf638f69f7fead
Andrés Achury <achuryjaramillo@gmail.com>
2021-05-23 23:29:36 -0500

    Refactor: Blog Home screen, section one

--- src/screens/Blog/home/HomeScreen.js
+++ src/screens/Blog/home/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import cta from '../../../assets/cta.jpg';
 // import women from '../../../assets/women.jpg';
+import butterfly from '../../../assets/butterfly.jpeg';
 // import {
 //   FaFacebookSquare,
 //   FaInstagramSquare,
@@ -9,7 +10,20 @@ import React from 'react';
 
 const HomeScreen = () => {
   return (
-    <div></div>
+    <main>
+      <section className="blog__screen__s1">
+        <h1>
+          Aprende, descubre <br /> y aplica como manejar tus emociones
+        </h1>
+        <p> Inicia ahora </p>
+      </section>
+      {/* <section className="home-screen__s__two">
+        <q>
+          <cite> - La vida es un mosaico de roles y experiencias - </cite>
+        </q>
+      </section> */}
+    </main>
+
     // <main>
     //   <section className="home-screen__s__one">
     //     <img src={cta} alt="home" title="Maneja tus emociones" />
