1ebf8487d9ab56c3d2c2b5089443d3ca1f9d8393
Andrés Achury <aachury@serempre.com>
2021-08-09 18:58:21 -0500

    Feat: Primary Color Added to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          500: "#6A5FDB",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
