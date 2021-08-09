cdf305eb2e9c593f9d3c6ea41476eefa2aff0115
Andrés Achury <aachury@serempre.com>
2021-08-09 17:24:44 -0500

    Chore: Delete unnecessary styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import BlogApp from "./BlogApp"
-// import './styles/styles.scss';
 import "./styles/tailwind.scss"
 
 ReactDOM.render(<BlogApp />, document.getElementById("root"))
