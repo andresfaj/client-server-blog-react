0405aaa124529fd06f9a5c659fc871a02837a5bf
Andrés Achury <aachury@serempre.com>
2021-08-09 17:28:51 -0500

    Refactor for Screens

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,30 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import BlogRouter from "./BlogRouter"
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom"
+import useScreens from "../screens"
 
 const AppRouter = () => {
+  const {
+    HomeScreen,
+    BlogScreen,
+    WorkShopScreen,
+    AboutMeScreen,
+    ContactScreen,
+  } = useScreens()
   return (
     <Router>
-      <div>
-        <Switch>
-          <Route path="/" component={BlogRouter} />
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path="/" component={HomeScreen} />
+        <Route exact path="/blog" component={BlogScreen} />
+        <Route exact path="/workshops" component={WorkShopScreen} />
+        <Route exact path="/aboutme" component={AboutMeScreen} />
+        <Route exact path="/contact" component={ContactScreen} />
+        <Redirect to="/" />
+      </Switch>
     </Router>
   )
 }
---
+++ src/screens/Blog/BlogScreen.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const BlogScreen = () => {
+  return <h1>Blog Screen</h1>
+}
+
+export default BlogScreen
