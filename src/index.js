d892c53f65fe3781ea369cd029deff3ba8d070e5
Andrés Achury <aachury@serempre.com>
2021-08-08 23:56:04 -0500

    Config TailwindCSS and CRACO

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import BlogApp from './BlogApp';
-import './styles/styles.scss';
+import React from "react"
+import ReactDOM from "react-dom"
+import BlogApp from "./BlogApp"
+// import './styles/styles.scss';
+import "./styles/tailwind.scss"
 
-ReactDOM.render(<BlogApp />, document.getElementById('root'));
+ReactDOM.render(<BlogApp />, document.getElementById("root"))
