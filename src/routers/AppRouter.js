8c9d92258783cc0f87361ae75786821a49974aad
Andrés Achury <aachury@serempre.com>
2021-08-09 15:45:40 -0500

    Chore: Delete unnecessary files

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import AuthRouter from './AuthRouter';
-import BlogRouter from './BlogRouter';
-import DashboardRouter from './DashboardRouter';
+import React from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import BlogRouter from "./BlogRouter"
 
 const AppRouter = () => {
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/auth" component={AuthRouter} />
-          <Route path="/dashboard" component={DashboardRouter} />
           <Route path="/" component={BlogRouter} />
         </Switch>
       </div>
     </Router>
-  );
-};
+  )
+}
 
-export default AppRouter;
+export default AppRouter
