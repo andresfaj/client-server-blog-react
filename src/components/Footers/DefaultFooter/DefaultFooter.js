6a79518861c2e12f9b64a6c975e6bcc8afeb4486
Andrés Achury <aachury@serempre.com>
2021-08-09 22:19:21 -0500

    Feat: Logic for default footer

--- src/components/Footers/DefaultFooter/DefaultFooter.js
+++ src/components/Footers/DefaultFooter/DefaultFooter.js
@@ -1,21 +1,59 @@
 //Packages
 import React from "react"
 import PropTypes from "prop-types"
+import _ from "lodash"
 
 //Styled Components
-import { StyledFooter } from "./DefaultFooter.styles"
+import {
+  StyledFooter,
+  StyledP,
+  StyledSocialLinksWrapper,
+} from "./DefaultFooter.styles"
+
+//Hooks
+import useHooks from "../../../hooks"
+import useComponents from "../.."
 
 const DefaultFooter = (props) => {
-  const { className } = props
+  const { className, socialLinks } = props
+
+  const { useSocialIcons } = useHooks()
+  const { useLinks } = useComponents()
+
+  const { socialIconHash } = useSocialIcons()
+  const { AppLink } = useLinks()
+
   return (
     <StyledFooter className={className}>
-      <h1>This ia a Footer</h1>
+      {socialLinks && socialLinks.length > 0 && (
+        <StyledSocialLinksWrapper>
+          {_.map(socialLinks, (link) => {
+            let SocialIcon = socialIconHash[link.name.toLowerCase()]
+            return (
+              <AppLink
+                key={link.id}
+                href={link.url}
+                target="_blank"
+                className="text-white"
+              >
+                <SocialIcon className="h-6 w-6" />
+              </AppLink>
+            )
+          })}
+        </StyledSocialLinksWrapper>
+      )}
+      <StyledP>Copyright &copy; 2021</StyledP>
     </StyledFooter>
   )
 }
 
 DefaultFooter.propTypes = {
   className: PropTypes.string,
+  socialLinks: PropTypes.array,
+}
+
+DefaultFooter.defaultProps = {
+  socialLinks: [],
 }
 
 export default DefaultFooter
--- src/components/Footers/DefaultFooter/DefaultFooter.styles.js
+++ src/components/Footers/DefaultFooter/DefaultFooter.styles.js
@@ -4,5 +4,16 @@ import tw from "twin.macro"
 export const StyledFooter = styled.footer.attrs({
   className: "StyledFooter",
 })`
-  ${tw`absolute bottom-0 w-full bg-primary-500 text-center`}
+  ${tw`absolute bottom-0 w-full bg-primary-500 text-center py-5`}
+`
+
+export const StyledP = styled.p.attrs({
+  className: "StyledP",
+})`
+  ${tw`font-bold text-white`}
+`
+export const StyledSocialLinksWrapper = styled.div.attrs({
+  className: "StyledSocialLinksWrapper",
+})`
+  ${tw`flex justify-center my-2`}
 `
