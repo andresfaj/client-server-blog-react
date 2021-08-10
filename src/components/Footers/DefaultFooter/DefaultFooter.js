134faaf6be67e8269b95f203b145a5a5d6dccd9e
Andrés Achury <aachury@serempre.com>
2021-08-09 19:08:20 -0500

    Feat: Style Components Added for Default Footer Component

--- src/components/Footers/DefaultFooter/DefaultFooter.js
+++ src/components/Footers/DefaultFooter/DefaultFooter.js
@@ -1,9 +1,17 @@
+//Packages
 import React from "react"
 import PropTypes from "prop-types"
 
+//Styled Components
+import { StyledFooter } from "./DefaultFooter.styles"
+
 const DefaultFooter = (props) => {
   const { className } = props
-  return <div className={className}>Default Footer</div>
+  return (
+    <StyledFooter className={className}>
+      <h1>This ia a Footer</h1>
+    </StyledFooter>
+  )
 }
 
 DefaultFooter.propTypes = {
---
+++ src/components/Footers/DefaultFooter/DefaultFooter.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+import tw from "twin.macro"
+
+export const StyledFooter = styled.footer.attrs({
+  className: "StyledFooter",
+})`
+  ${tw`absolute bottom-0 w-full bg-primary-500 text-center`}
+`
