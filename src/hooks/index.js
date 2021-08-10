ea77a5256d4532eacd5574e3fc934dcc467af91f
Andrés Achury <aachury@serempre.com>
2021-08-09 22:23:30 -0500

    Feat: Custom Hook for use the social network icon

---
+++ src/hooks/index.js
@@ -0,0 +1,7 @@
+import useSocialIcons from "./useSocialIcons/useSocialIcons"
+
+const useHooks = () => {
+  return { useSocialIcons }
+}
+
+export default useHooks
---
+++ src/hooks/useSocialIcons/useSocialIcons.js
@@ -0,0 +1,13 @@
+import { FaFacebook, FaInstagram, FaYoutube, FaTwitter } from "react-icons/fa"
+
+const useSocialIcons = () => {
+  const socialIconHash = {
+    facebook: FaFacebook,
+    instagram: FaInstagram,
+    youtube: FaYoutube,
+    twitter: FaTwitter,
+  }
+  return { socialIconHash }
+}
+
+export default useSocialIcons
