03e560a728e07f2f207dd42cea8037233bba4bb8
Andrés Achury <aachury@serempre.com>
2021-08-09 23:59:09 -0500

    WIP default layout

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -5,6 +5,14 @@ import React from "react"
 //Hooks
 import useComponents from "../../components"
 
+const navLinks = [
+  { id: 1, name: "Inicio" },
+  { id: 2, name: "Blog" },
+  { id: 3, name: "Cursos y talleres" },
+  { id: 4, name: "Sobre mi" },
+  { id: 5, name: "Contáctame" },
+]
+
 const socialLinks = [
   { id: 1, url: "https://www.facebook.com/naydujaramillo", name: "facebook" },
   {
@@ -28,7 +36,7 @@ const DefaultLayout = (props) => {
 
   return (
     <div className="h-screen">
-      <DefaultHeader />
+      <DefaultHeader navLinks={navLinks} />
       {children}
       <DefaultFooter socialLinks={socialLinks} />
     </div>
