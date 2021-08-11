42cad275b64f180d38282cf29572428ef2d00793
Andrés Achury <aachury@serempre.com>
2021-08-10 22:16:58 -0500

    Chore: Prettier

--- src/BlogApp.js
+++ src/BlogApp.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import AppRouter from './routers/AppRouter';
+import React from "react";
+import AppRouter from "./routers/AppRouter";
 
 const BlogApp = () => {
   return <AppRouter />;
